bd6392fc1cbf9e39d9430c7f7e887994b204f724
Rizki Ashari <rizkiashari1026@gmail.com>
2024-11-27 12:55:06 +0700

    feat: done answer 4

--- src/app/(dashboard)/answer-four/page.tsx
+++ src/app/(dashboard)/answer-four/page.tsx
@@ -1,47 +1,26 @@
 'use client'
 
-import { useState, type FormEvent } from 'react'
+import { useEffect, useState, type FormEvent } from 'react'
 
-import { FormControl, Grid, Input, MenuItem, Select, TextareaAutosize, TextField, Typography } from '@mui/material'
+import { Alert, Grid, Input, MenuItem, Select, Typography } from '@mui/material'
 
-import { DatePicker , LoadingButton } from '@mui/lab'
-
-
+import { LoadingButton } from '@mui/lab'
 
 import FormWithCustom from '@/views/widgets/FormWithCustom'
 import { employees } from '@/data/payroll/employess'
+import CustomTextField from '@/@core/components/mui/TextField'
+import AppReactDatepicker from '@/lbs/styles/AppReactDatepicker'
 
 type InputFormType = {
   employeeId: number | undefined
   destination: string
-  departureDate: string
-  returnDate: string
   reason: string
 }
 
 export default function Page() {
-  /**
-Gunakan komponen Form untuk membuat form permintaan perjalanan dinas. Field:
-  - Nama Karyawan (dropdown dari API /api/employees)
-  - Tujuan (input teks)
-  - Tanggal Keberangkatan dan Kepulangan (date picker)
-  - Alasan Perjalanan (textarea)
-Validasi:
-  1. Nama Karyawan dan Tujuan wajib diisi.
-  2. Tanggal Keberangkatan tidak boleh sebelum hari ini.
-  3. Tanggal Kepulangan harus setelah Tanggal Keberangkatan.
-Data yang valid dikirim ke API berikut:
-    json
-    Salin kode
-    POST /api/travel-requests
-    {
-      "employeeId": 123,
-      "destination": "Jakarta",
-      "departureDate": "2024-11-25",
-      "returnDate": "2024-11-28",
-      "reason": "Konferensi HR"
-    }
-   */
+  const [dateDeparture, setDateDeparture] = useState<Date | null | undefined>(null)
+  const [dateReturn, setDateReturn] = useState<Date | null | undefined>(null)
+
   const [errors, setError] = useState({
     employeeId: '',
     destination: '',
@@ -54,8 +33,6 @@ Data yang valid dikirim ke API berikut:
   const [data, setData] = useState<InputFormType>({
     employeeId: undefined,
     destination: '',
-    departureDate: '',
-    returnDate: '',
     reason: ''
   })
 
@@ -63,13 +40,83 @@ Data yang valid dikirim ke API berikut:
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
+
+    if (!data.employeeId) {
+      return setError({ ...errors, employeeId: 'Nama Karyawan harus diisi' })
+    }
+
+    if (!data.destination) {
+      return setError({ ...errors, destination: 'Tujuan harus diisi' })
+    }
+
+    if (!dateDeparture) {
+      return setError({ ...errors, departureDate: 'Tanggal Keberangkatan harus diisi' })
+    }
+
+    if (!dateReturn) {
+      return setError({ ...errors, returnDate: 'Tanggal Kepulangan harus diisi' })
+    }
+
+    if (dateDeparture && dateDeparture < new Date()) {
+      return setError({ ...errors, departureDate: 'Tanggal Keberangkatan tidak boleh sebelum hari ini' })
+    }
+
+    if (dateDeparture && dateReturn && dateDeparture > dateReturn) {
+      return setError({ ...errors, returnDate: 'Tanggal Kepulangan harus setelah Tanggal Keberangkatan' })
+    }
+
+    setLoading(true)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+
+    const dataNew = {
+      employeeId: data.employeeId,
+      destination: data.destination,
+      departureDate: dateDeparture?.toISOString(),
+      returnDate: dateReturn?.toISOString(),
+      reason: data.reason
+    }
+
+    console.log(dataNew)
+
+    setMessageSuccess('Data berhasil disimpan')
+
+    setTimeout(() => {
+      setData({
+        employeeId: undefined,
+        destination: '',
+        reason: ''
+      })
+      setDateDeparture(null)
+      setDateReturn(null)
+      setMessageSuccess('')
+    }, 2200)
   }
+
+  useEffect(() => {
+    // Reset error message when data is changed
+    setTimeout(() => {
+      setError({
+        employeeId: '',
+        destination: '',
+        departureDate: '',
+        returnDate: ''
+      })
+    }, 1000)
+  }, [data, dateDeparture, dateReturn])
 
   return (
     <>
       <Typography variant='h2' className='mbe-3'>
         Soal 4: Form Permintaan Perjalanan Dinas
       </Typography>
+      {messageSuccess && (
+        <Alert severity='success' className='mbe-3'>
+          {messageSuccess}
+        </Alert>
+      )}
       <form onSubmit={handleSubmit}>
         <Grid container spacing={6} className='mbe-3'>
           <Grid item xs={12} md={6}>
@@ -100,28 +147,50 @@ Data yang valid dikirim ke API berikut:
             </FormWithCustom>
           </Grid>
           <Grid item xs={12} md={6}>
-            <DatePicker
-              value={data.departureDate}
-              onChange={(date: any) => setData({ ...data, departureDate: date })}
-              renderInput={() => (
-                <TextField label='Tanggal Keberangkatan' name='departureDate' helperText={errors.departureDate} />
-              )}
+            <AppReactDatepicker
+              selected={dateDeparture}
+              placeholderText='YYYY-MM-DD'
+              dateFormat={'yyyy-MM-dd'}
+              onChange={(date: Date | null) => setDateDeparture(date)}
+              customInput={
+                <CustomTextField
+                  fullWidth
+                  label='Tanggal Keberangkatan'
+                  FormHelperTextProps={{ error: true }}
+                  helperText={errors.departureDate}
+                />
+              }
             />
           </Grid>
           <Grid item xs={12} md={6}>
-            <DatePicker
-              value={data.returnDate}
-              onChange={(date: any) => setData({ ...data, returnDate: date })}
-              renderInput={() => (
-                <TextField label='Tanggal Kepulangan' name='returnDate' helperText={errors.returnDate} />
-              )}
+            <AppReactDatepicker
+              selected={dateReturn}
+              placeholderText='YYYY-MM-DD'
+              dateFormat={'yyyy-MM-dd'}
+              onChange={(date: Date | null) => setDateReturn(date)}
+              customInput={
+                <CustomTextField
+                  label='Tanggal Kepulangan'
+                  fullWidth
+                  FormHelperTextProps={{ error: true }}
+                  helperText={errors.returnDate}
+                />
+              }
             />
           </Grid>
           <Grid item xs={12} md={6}>
-            <FormWithCustom name='reason' label='Alasan Perjalanan'>
-              <TextareaAutosize name='reason' id='reason' />
-            </FormWithCustom>
+            <CustomTextField
+              rows={4}
+              multiline
+              name='reason'
+              fullWidth
+              label='Alasan Perjalanan'
+              id='reason'
+              value={data.reason}
+              onChange={e => setData({ ...data, reason: e.target.value })}
+            />
           </Grid>
+          <Grid item xs={12} md={6} />
           <Grid item xs={12} md={6}>
             <LoadingButton
               type='submit'
